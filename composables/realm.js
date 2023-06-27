d01ba863f59abd32fff7a9026ec3d519dfc5de4c
Soumesh <iamsoumo26@gmail.com>
2023-06-27 12:59:24 +0530

    realm composable added

---
+++ composables/realm.js
@@ -0,0 +1,14 @@
+import * as Realm from 'realm-web'
+
+export function useRealm(){
+    const app = new Realm.App({
+        id: useRuntimeConfig().public.appId
+    })
+    let mongo=null
+    if(app.currentUser){
+        mongo=app.currentUser.mongoClient('mongodb-atlas')
+    }
+    return {
+        app, Realm,mongo
+    }
+}   
