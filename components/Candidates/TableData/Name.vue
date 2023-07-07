e47b559d319331707956b79256b2561c44d104db
Soumesh Kundu <iamsoumo26@gmail.com>
2023-07-07 16:20:24 +0530

    bug fixed

--- components/Candidates/TableData/Name.vue
+++ components/Candidates/TableData/Name.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center px-3 py-5 text-gray-900 w-[170px] truncate">
-        <img class="w-10 h-10 rounded-full" :src="content.image ?? defaultImg" alt="Jese image" />
+        <img class="w-10 h-10 rounded-full" :src="content?.image ?? defaultImg" alt="Jese image" />
 
         <p class="pl-3 truncate">{{ content}}</p>
     </div>
--- components/Candidates/TableData/Stages.vue
+++ components/Candidates/TableData/Stages.vue
@@ -2,18 +2,18 @@
     <div class="px-3 py-4">
         <div class="flex items-center">
             <button class="flex items-center gap-1">
-                <span class="text-black">{{ content.state }}</span>
+                <span class="text-black">{{ content?.state }}</span>
                 <ChevronDownIcon class="w-4 h-4 font-semibold text-black" />
             </button>
         </div>
 
-        <div v-if="content.value <= 6" class="flex items-center gap-1 mt-1">
-            <div v-for="n in content.value" :key="n"
-                :class="`bg-${colorMap.get(content.state)} w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm ` ">
+        <div v-if="content?.value <= 6" class="flex items-center gap-1 mt-1">
+            <div v-for="n in content?.value" :key="n"
+                :class="`bg-${colorMap.get(content?.state)} w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm ` ">
                 {{ n }}
             </div>
 
-            <div v-for="n in 6 - content.value" :key="n"
+            <div v-for="n in 6 - content?.value" :key="n"
                 class="flex items-center justify-center text-sm font-medium text-white bg-gray-200 rounded-sm w-7">
                 &nbsp;
             </div>
